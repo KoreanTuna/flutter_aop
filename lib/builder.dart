bc9a6b64d4be2ed94756113de8fc767e752869ae
KoreanTuna <songsee0528@gmail.com>
2025-11-20 12:59:53 +0900

    🩹 modify build for lint generated file warning

--- lib/builder.dart
+++ lib/builder.dart
@@ -3,5 +3,14 @@ import 'package:source_gen/source_gen.dart';
 
 import 'src/generator/aop_generator.dart';
 
-Builder aopBuilder(BuilderOptions options) =>
-    PartBuilder([AopGenerator()], '.aop.dart');
+const _generatedFileHeader = '''
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// dart format width=80
+''';
+
+Builder aopBuilder(BuilderOptions options) => PartBuilder(
+      [AopGenerator()],
+      '.aop.dart',
+      header: _generatedFileHeader,
+    );
